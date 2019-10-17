c95a48fbe3385d6c938c44e7e78ce59d6d40dacf
luanBrandao <luanbrandao4@gmail.com>
2019-10-17 12:18:14 -0300

    config: eslint

--- API/src/app.js
+++ API/src/app.js
@@ -1,27 +1,30 @@
-import  express from 'express';
-import  rotues from './routes';
+import express from 'express';
+import rotues from './routes';
 
 // yarn init -y
 // yarn add express
 // para usar import
 // yarn add sucrase nodemon -D
+// yarn add eslint -D
+// yarn eslint --init
+// deixa o codigo bonito kk
+// yarn add prettier eslint-config-prettier eslint-plugin-prettier -D
+// todos os arquivos dentro da pasta src sao ajustados com o eslint automaticamente
+// yarn eslint --fix src --ext .js
 class App {
- constructor() {
-   this.server = express();
-   this.middlewares();
-   this.routes();
- }
+  constructor() {
+    this.server = express();
+    this.middlewares();
+    this.routes();
+  }
 
- middlewares(){
-  this.server.use(express.json());
- }
+  middlewares() {
+    this.server.use(express.json());
+  }
 
- routes(){
-  this.server.use(rotues)
- }
-
- 
+  routes() {
+    this.server.use(rotues);
+  }
 }
 
-export default  new App().server;
-
+export default new App().server;
--- API/src/routes.js
+++ API/src/routes.js
@@ -1,9 +1,7 @@
-import { Router  } from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>{
-    return res.json({hello : 'hello ok'})
-});
+routes.get('/', (req, res) => res.json({ hello: 'hello ok' }));
 
-export default  routes;
+export default routes;
