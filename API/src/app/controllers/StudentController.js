5a2bbc2eb874e29388374e451d0592f29dcff4dd
luanBrandao <luanbrandao4@gmail.com>
2019-10-18 17:30:28 -0300

    feat: update Student

--- API/src/app/controllers/StudentController.js
+++ API/src/app/controllers/StudentController.js
@@ -15,6 +15,34 @@ class StudentController {
       return res.json({ name, email, phone, height, weight, date_birth, active,});
   }
 
+  async update(req,res) {
+
+    const { id , email } = req.body;
+
+    if (!id) {
+      return res.status(401).json({ error: 'Id required' });
+    }
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(401).json({ error: 'Student not exist' });
+    }
+
+    if (email !== student.email) {
+      const studentExists = await Student.findOne({
+        where: { email: req.body.email },
+      });
+      if (studentExists) {
+        return res.status(400).json({ error: 'Email already exists.' });
+      }
+    }
+
+    const { name , phone, height, weight, date_birth, active,} = await student.update(req.body);
+    return res.json({ name ,email, phone, height, weight, date_birth, active,});
+  }
+
+
 }
 
 export default new StudentController();
--- API/src/routes.js
+++ API/src/routes.js
@@ -30,5 +30,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
 
 export default routes;
