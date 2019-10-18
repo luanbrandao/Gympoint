af6fb9f63bbcd42301b89638e4dd69d533ea01f5
luanBrandao <luanbrandao4@gmail.com>
2019-10-18 13:31:45 -0300

    feat: create model Student

---
+++ API/src/app/models/Student.js
@@ -0,0 +1,24 @@
+import { Model, Sequelize } from 'sequelize';
+
+class Student extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        // os dados que podem ser enviados
+        // o model não precisa ter os mesmos campos do db
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        phone: Sequelize.STRING,
+        height: Sequelize.DOUBLE,
+        weight: Sequelize.DOUBLE,
+        age: Sequelize.INTEGER,
+        active: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
+  }
+}
+
+export default Student;
--- API/src/routes.js
+++ API/src/routes.js
@@ -3,20 +3,24 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import Student from './app/models/Student';
 
 const routes = new Router();
 
 // routes.get('/', (req, res) => res.json({ message: 'hello Rocktseat!' }));
 
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Diego',
-//     email: 'deigo@gmail.com',
-//     phone: '123123',
-//     password_hash: '123123',
-//   });
-//   return res.json(user);
-// });
+routes.get('/students', async (req, res) => {
+  const student = await Student.create({
+    name: '1231',
+    email: '1231@gmail.com',
+    phone: '123123',
+    height: 2.3,
+    weight: 1.2,
+    age: 12,
+    active: true,
+  });
+  return res.json(student);
+});
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
