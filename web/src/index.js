a0bee7d3f9fece4d7dfd1bf613b067d2b28a6c94
luanBrandao <luanbrandao4@gmail.com>
2019-12-02 03:42:36 -0300

    WEB - ESLint, Prettier & EditorConfig

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
+// yarn add eslint -D
+// yarn eslint --init
+// yarn eslint --init
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// yarn add prettier eslint-config-prettier eslint-plugin-prettier babel-eslint  -D
+// yarn add eslint-plugin-react-hooks -D
+
+// yarn add react-router-dom
+// yarn add history
+
+// yarn add reactotron-react-js
+
+// yarn add prop-types
+// yarn add eslint-plugin-jsx-a11y
+// yarn add eslint-plugin-import
+
+// yarn add styled-components
+
+// alterar os imports usndo ../..
+// yarn add customize-cra react-app-rewired -D
+// yarn add babel-plugin-root-import -D
+// yarn add eslint-import-resolver-babel-plugin-root-import -D
+
+// yarn add polished
+// yarn add @rocketseat/unform
+
+// yarn add yup
+
+// yarn add redux  redux-saga react-redux reactotron-redux reactotron-redux-saga reactotron-react-js immer
+
+// yarn add axios
+// yarn add redux-persist
+
+// yarn add react-toastify
+// yarn add react-icons
+// yarn add react-perfect-scrollbar
+// yarn add date-fns@next
+
+// yarn add date-fns-tz
+
+ReactDOM.render(<App />, document.getElementById("root"));
