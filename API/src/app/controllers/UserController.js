408a82294d0b8a82019ad666f1edd825897fb77a
luanBrandao <luanbrandao4@gmail.com>
2019-12-03 10:22:45 -0300

    API - return avatar user session and update user

--- API/src/app/controllers/UserController.js
+++ API/src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -62,9 +63,22 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, phone, provider } = await user.update(req.body);
+    // const { id, name, phone, provider } = await user.update(req.body);
+    await user.update(req.body);
+    const { id, name, phone, provider, avatar } = await User.findByPk(
+      req.userId,
+      {
+        include: [
+          {
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+          },
+        ],
+      }
+    );
 
-    return res.json({ id, name, email, phone, provider });
+    return res.json({ id, name, email, phone, provider, avatar });
   }
 }
 
