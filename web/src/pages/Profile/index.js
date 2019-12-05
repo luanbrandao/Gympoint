56406feeedc9ff41209e468648fef1a62e4db174
luanBrandao <luanbrandao4@gmail.com>
2019-12-05 19:09:54 -0300

    WEB  - templat page profile

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  return (
+    <Container>
+      <Form initialData={profile}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereçode e-mail" />
+        <Input name="phone" placeholder="Seu telefone" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair GymPoint</button>
+    </Container>
+  );
 }
