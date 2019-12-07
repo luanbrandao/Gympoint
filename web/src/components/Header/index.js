1f5b644ed49fd6d89bbc4bfef47f5fe0c9c84a12
luanBrandao <luanbrandao4@gmail.com>
2019-12-07 15:11:23 -0300

    WEB -  Dashboard_Plans

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -18,7 +18,7 @@ export default function Header() {
       <Content>
         <nav>
           <img src={logo} alt="GymPoint" />
-          <Link to="/dashboard">ALUNOS</Link>
+          <Link to="/dashboard_student">ALUNOS</Link>
           <Link to="/dashboard">PLANOS</Link>
           <Link to="/dashboard">MATRÍCULAS</Link>
           <Link to="/dashboard">PEDIDOS DE AUXÍLIO</Link>
