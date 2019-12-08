54e80716b4cfe0a4d07971ebbaf16e358fc8fe1b
luanBrandao <luanbrandao4@gmail.com>
2019-12-08 14:36:15 -0300

    WEB - Dashboard_Students

--- web/src/pages/Dashboard_Help_Orders/index.js
+++ web/src/pages/Dashboard_Help_Orders/index.js
@@ -5,15 +5,15 @@ import { Container, Header, Main, Table, Delete } from './styles';
 import api from '~/services/api';
 
 export default function Dashboard_Students() {
-  const [students, setStudents] = useState([]);
+  const [helpOrders, setHelpOrders] = useState([]);
 
   useEffect(() => {
     async function loadStudents() {
-      const response = await api.get('students');
+      const response = await api.get('help_orders');
       // executa a formatação assim que pega os dados da api
       // para executar apenas uma unica vez
       const { data } = response;
-      setStudents(data.students);
+      setHelpOrders(data.help_orders);
     }
 
     loadStudents();
@@ -36,9 +36,9 @@ export default function Dashboard_Students() {
           </thead>
 
           <tbody>
-            {students.map(student => (
-              <tr key={student.name}>
-                <td>{student.name}</td>
+            {helpOrders.map(helpOrder => (
+              <tr key={helpOrder.id}>
+                <td>{helpOrder.student.name}</td>
                 <td>
                   <Delete type="button">
                     responder
