76abb317803449fb925c76abec11b7b27e4105e6
luanBrandao <luanbrandao4@gmail.com>
2019-12-17 01:18:36 -0300

    APP - create project

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <Text>OK</Text>
+    </>
+  );
+};
+
+export default App;
