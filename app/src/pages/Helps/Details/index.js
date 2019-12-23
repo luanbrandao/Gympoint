f63833c624a2ec585f4915d165c7ae27821f9da4
luanBrandao <luanbrandao4@gmail.com>
2019-12-23 01:53:31 -0300

    APP - page Helps/Details

--- app/src/pages/Helps/Details/index.js
+++ app/src/pages/Helps/Details/index.js
@@ -1,13 +1,62 @@
-import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, { useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+import {
+  Container,
+  Question,
+  Header,
+  Answer,
+  Title,
+  Time,
+  Message,
+} from './styles';
 
 export default function Details({ navigation }) {
   const help = navigation.getParam('help');
+
+  const dateQuestion = useMemo(() => {
+    return formatRelative(parseISO(help.createdAt), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [help.createdAt]);
+
+  let dateAnswer;
+
+  if (help.answer_at) {
+    dateAnswer = useMemo(() => {
+      return formatRelative(parseISO(help.answer_at), new Date(), {
+        locale: pt,
+        addSuffix: true,
+      });
+    }, [help.createdAt]);
+  }
   console.tron.log('help => ', help);
-  return <Background />;
+  return (
+    <Background>
+      <Container>
+        <Question>
+          <Header>
+            <Title>Pertunta</Title>
+            <Time>{dateQuestion}</Time>
+          </Header>
+          <Message>{help.question}</Message>
+        </Question>
+        <Answer>
+          <Header>
+            <Title>Resposta</Title>
+            <Time>{help.answer_at && dateAnswer ? dateAnswer : ''}</Time>
+          </Header>
+          <Message>
+            {help.answer ? help.answer : 'Aguardando resposta!'}
+          </Message>
+        </Answer>
+      </Container>
+    </Background>
+  );
 }
 
 Details.navigationOptions = ({ navigation }) => ({
---
+++ app/src/pages/Helps/Details/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  background: #fff;
+  flex: 1;
+  margin: 50px 15px 10px 15px;
+  border-radius: 4px;
+  padding: 10px;
+`;
+
+export const Question = styled.View``;
+export const Answer = styled.View``;
+
+export const Header = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 20px 0 20px 0;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const Time = styled.Text``;
+export const Message = styled.Text``;
