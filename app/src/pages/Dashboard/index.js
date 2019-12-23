feeef2b9c8174e32f1c10614ffd1d78b523ddf4b
luanBrandao <luanbrandao4@gmail.com>
2019-12-22 23:49:14 -0300

    APP - add page Dashboard isFocused

--- app/src/pages/Dashboard/index.js
+++ app/src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Image } from 'react-native';
+import { withNavigationFocus } from 'react-navigation';
 import Background from '~/components/Background';
 import { Container, Header, List, BtnCheckIn } from './styles';
 import CheckIn from '~/components/CheckIn';
@@ -8,17 +9,20 @@ import logo from '~/assets/logo.png';
 import api from '~/services/api';
 import { store } from '~/store';
 
-export default function Dashboard() {
+function Dashboard({ isFocused }) {
   const [checkIns, setCheckIns] = useState([]);
   const { student } = store.getState().auth;
 
+  async function loadCheckIns() {
+    const response = await api.get(`students/${student.id}/checkins`);
+    setCheckIns(response.data.checkins);
+  }
+
   useEffect(() => {
-    async function loadCheckIns() {
-      const response = await api.get(`students/${student.id}/checkins`);
-      setCheckIns(response.data.checkins);
+    if (isFocused) {
+      loadCheckIns();
     }
-    loadCheckIns();
-  }, []);
+  }, [isFocused]);
 
   return (
     <Background>
@@ -44,3 +48,5 @@ Dashboard.navigationOptions = {
     <Icon name="check" size={20} color={tintColor} />
   ),
 };
+
+export default withNavigationFocus(Dashboard);
